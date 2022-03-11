780b121a99a5f5ed533ebbc7e1492b0c70d6bc30
Uycoder <ahmedbughra@gmail.com>
2022-03-11 23:12:02 +0300

    add dependencies

---
+++ graduationproject/src/main/java/dev/ahmed/graduationproject/GraduationprojectApplication.java
@@ -0,0 +1,13 @@
+package dev.ahmed.graduationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraduationprojectApplication.class, args);
+    }
+
+}
